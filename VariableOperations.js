a71b169887a086afb78f8de7cd1a8839fa3f50a5
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-09-02 14:49:21 +0300

    Add files via upload

---
+++ VariableOperations.js
@@ -0,0 +1,20 @@
+var a = 3;
+var b = 17;
+var c =12 ;
+ // only modify this code below this line
+
+ a+=12;
+ b+=9;
+ c+=7;
+ //shorcuts subtrating variables
+ a-=6;
+ b-=15;
+ c-=1;
+//Shorcuts to multiplying variables
+ a*=5;
+ b*=3;
+ c*=10;
+ //shortcuts to dividing variables
+ a /=12;
+ b/=4;
+ c/=11;
